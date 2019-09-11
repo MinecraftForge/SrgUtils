b8a577a4d6aa9d0b174ae7e36b5289eba7bc6f67
LexManos <LexManos@gmail.com>
2019-09-11 12:02:41 -0700

    Inital Commit

---
+++ src/test/java/net/minecraftforge/srgutils/test/MappingTest.java
@@ -0,0 +1,46 @@
+/*
+ * SRG Utils
+ * Copyright (c) 2019
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.minecraftforge.srgutils.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+
+import net.minecraftforge.srgutils.IMappingFile;
+import net.minecraftforge.srgutils.IMappingFile.Format;
+
+public class MappingTest {
+
+    InputStream getStream(String name) {
+        return MappingTest.class.getClassLoader().getResourceAsStream(name);
+    }
+
+    @Test
+    void test() throws IOException {
+        IMappingFile pg = IMappingFile.load(getStream("./installer.pg"));
+        for (Format f : Format.values()) {
+            pg.write(Paths.get("./build/installer_out." + f.name().toLowerCase()), f, false);
+        }
+    }
+}
