917096ea6a3a8087c564d90a86c8ec236f9a96c6
LexManos <LexManos@gmail.com>
2024-04-15 22:04:21 -0700

    Potatoe

--- src/main/java/net/minecraftforge/srgutils/MinecraftVersion.java
+++ src/main/java/net/minecraftforge/srgutils/MinecraftVersion.java
@@ -52,7 +52,12 @@ public class MinecraftVersion implements Comparable<MinecraftVersion> {
         if (value == 2224)                  return "1.19.1";
         if (value >= 2242 && value <= 2246) return "1.19.3";
         if (value >= 2303 && value <= 2307) return "1.19.4";
-        if (value >= 2312 && value <= 9999) return "1.20";
+        if (value >= 2312 && value <= 2318) return "1.20";
+        if (value >= 2320 && value <= 2330) return "1.20.1";
+        if (value >= 2331 && value <= 2341) return "1.20.2";
+        if (value >= 2342 && value <= 2346) return "1.20.3";
+        if (value >= 2347 && value <= 2350) return "1.20.4";
+        if (value >= 2351 && value <= 9999) return "1.20.5";
         throw new IllegalArgumentException("Invalid snapshot date: " + value);
     }
 
@@ -99,6 +104,8 @@ public class MinecraftVersion implements Comparable<MinecraftVersion> {
             return new MinecraftVersion(Type.APRIL_FOOLS, version, 13, 22, 0, "b", splitDots("1.19"));
         else if ("23w13a_or_b".equals(lower))          // 2023 April Fools
             return new MinecraftVersion(Type.APRIL_FOOLS, version, 13, 23, 0, "b", splitDots("1.20"));
+        else if ("24w14potato".equals(lower))          // 2024 April Fools
+            return new MinecraftVersion(Type.APRIL_FOOLS, version, 14, 24, 0, "b", splitDots("1.20.5"));
         else if ("inf-20100618".equals(lower))
             return new MinecraftVersion(Type.ALPHA, version, 25, 10, 0, "a", splitDots("1.0.4"));
         else if ("c0.0.13a_03".equals(lower))          // Rather than screw with the logic of the alpha/beta parser, special case this weird one
